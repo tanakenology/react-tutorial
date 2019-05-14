1df0408f63bb41bf9d53f9d877e9a65541d217e8
tanakenology <x27c5343bd78n@gmail.com>
2019-05-14 20:18:22 +0900

    Fix

--- src/index.js
+++ src/index.js
@@ -15,9 +15,9 @@ class Square extends React.Component {
     return (
       <button
         className="square"
-        onClick={() => this.setState({value: 'X'})}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -31,8 +31,19 @@ class Board extends React.Component {
     }
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
