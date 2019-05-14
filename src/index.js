9cde5186c9484ee8392d4592502498eebe064606
tanakenology <x27c5343bd78n@gmail.com>
2019-05-14 14:00:50 +0900

    Fix

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { nullLiteral } from '@babel/types';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return <Square value={this.state.squares[i]} />;
   }
 
   render() {
