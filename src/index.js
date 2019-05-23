c76d46183195c794b3a57239ff43ac6b0e55c048
tanakenology <x27c5343bd78n@gmail.com>
2019-05-23 21:12:17 +0900

    Use two loops to show board

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
+        key={i}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
@@ -23,21 +24,15 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {[...Array(3).keys()].map(n => {
+          return (
+            <div className="board-row" key={n}>
+              {[...Array(3).keys()].map(m => {
+                return this.renderSquare(n*3+m)
+              })}
+            </div>
+          )
+        })}
       </div>
     );
   }
